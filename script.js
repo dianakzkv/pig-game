69a4d17df4136246648432163d81899eb28aff20
Diana <dianushka.di.jesse@gmail.com>
2023-10-27 13:51:49 +0300

    info btn add

--- script.js
+++ script.js
@@ -89,4 +89,23 @@ buttonHold.addEventListener('click', function () {
 
 buttonNew.addEventListener('click', init);
 
-buttonInfo.addEventListener('click', function () {});
+const closeModal = function () {
+  document.querySelector(`.modal`).classList.add('hidden');
+  document.querySelector(`.overlay`).classList.add('hidden');
+};
+
+buttonInfo.addEventListener('click', function () {
+  document.querySelector(`.modal`).classList.remove('hidden');
+  document.querySelector(`.overlay`).classList.remove('hidden');
+});
+
+document.querySelector('.close-modal').addEventListener('click', closeModal);
+document.querySelector('.overlay').addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (
+    e.key === 'Escape' &&
+    !document.querySelector(`.modal`).classList.contains('hidden')
+  ) {
+    closeModal();
+  }
+});
